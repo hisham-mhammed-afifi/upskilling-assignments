27a2c7e0ddba320ebfe95a9a5a10f46a9caf7ad0
hesham afifi <hisham.mhammed.afifi@gmail.com>
2025-02-23 03:59:11 +0200

    advanced-course - collections

---
+++ Advanced.Collections/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced.Collections
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Grade { get; set; }
+
+        public Student(int id, string name, double grade)
+        {
+            Id = id;
+            Name = name;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Grade: {Grade}";
+        }
+    }
+}
